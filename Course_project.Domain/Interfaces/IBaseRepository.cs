e85937bf05febbe1032e4ab029f19f37eef1d024
Сергатий Євгеній <evgensergatiy@gmail.com>
2022-08-27 23:28:57 +0300

    Made some tests and additional changes at AddRep

--- Course_project.Domain/Interfaces/IBaseRepository.cs
+++ Course_project.Domain/Interfaces/IBaseRepository.cs
@@ -8,11 +8,11 @@ namespace Course_project.DAL.Interfaces
 {
     public interface IBaseRepository<T> where T : class
     {
-        T Get(int id);
+        Task<T> Get(int id);
         Task<List<T>> GetAll();
-        bool Create(T entity);
-        bool Delete(int id);
-        bool Update(T entity);
+        Task<bool> Create(T entity);
+        Task<bool> Delete(T entity);
+        Task<bool> Update(T entity);
 
     }
 }
